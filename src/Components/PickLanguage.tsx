90c74d114e59ab32380127a9a6acd562de6ab20d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-22 18:47:55 +0100

    fix typo

--- src/Components/PickLanguage.tsx
+++ src/Components/PickLanguage.tsx
@@ -3,8 +3,8 @@ const PickLanguage = (props: { setLanguage: (arg0: string) => void }) => (
     <h1>Welcome To Unlimited Wordle!</h1>
     <p>Please select your language:</p>
     <div>
-      <button onClick={() => props.setLanguage("en")}>العربية</button>
-      <button onClick={() => props.setLanguage("ar")}>English</button>
+      <button onClick={() => props.setLanguage("ar")}>العربية</button>
+      <button onClick={() => props.setLanguage("en")}>English</button>
       <button onClick={() => props.setLanguage("fr")}>Français</button>
     </div>
   </div>
