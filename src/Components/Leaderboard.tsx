732b0627e62c0ea05dbd87e78572c194aeb9075a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-22 20:39:27 +0100

    Add leaderboard component

---
+++ src/Components/Leaderboard.tsx
@@ -0,0 +1,54 @@
+const Leaderboard = () => {
+  const leaderboard = [
+    {
+      name: "John Doe",
+      score: 100,
+    },
+    {
+      name: "Jane Doe",
+      score: 90,
+    },
+    {
+      name: "Joe Doe",
+      score: 80,
+    },
+    {
+      name: "Jill Doe",
+      score: 70,
+    },
+    {
+      name: "Jack Doe",
+      score: 60,
+    },
+    {
+      name: "Jill Doe",
+      score: 50,
+    },
+    {
+      name: "Jack Doe",
+      score: 40,
+    },
+  ];
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard.map(player => (
+            <tr key={player.name}>
+              <td>{player.name}</td>
+              <td>{player.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default Leaderboard;
