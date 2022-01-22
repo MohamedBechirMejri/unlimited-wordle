2d4c98dd94608b20edd34ce7bb18bec462d45cd3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-22 19:18:45 +0100

    add Header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,42 @@
+import {
+  MdHelpOutline,
+  MdOutlineLeaderboard,
+  MdOutlineSettings,
+  MdClose,
+} from "react-icons/md";
+const Header = ({
+  page,
+  setPage,
+}: {
+  page: string;
+  setPage: (arg0: string) => void;
+}) => {
+  return (
+    <div>
+      <h1>UW</h1>
+      <button
+        onClick={() => {
+          setPage(page === "info" ? "" : "info");
+        }}
+      >
+        {page === "info" ? <MdClose /> : <MdHelpOutline />}
+      </button>
+      <button
+        onClick={() => {
+          setPage(page === "leaderboard" ? "" : "leaderboard");
+        }}
+      >
+        {page === "leaderboard" ? <MdClose /> : <MdOutlineLeaderboard />}
+      </button>
+      <button
+        onClick={() => {
+          setPage(page === "settings" ? "" : "settings");
+        }}
+      >
+        {page === "settings" ? <MdClose /> : <MdOutlineSettings />}
+      </button>
+    </div>
+  );
+};
+
+export default Header;
