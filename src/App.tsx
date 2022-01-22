9078368a80fc2eab409d44bf7758ab6308a1fb7e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-22 18:25:33 +0100

    add language context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import React, { useState } from "react";
+import PickLanguage from "./Components/PickLanguage";
 const App = () => {
-  return <div className=""></div>;
+  const [language, setLanguage] = useState("");
+  const LanguageContext = React.createContext(language);
+
+  return (
+    <LanguageContext.Provider value={language}>
+      <div className="">
+        {language && <PickLanguage setLanguage={setLanguage} />}
+      </div>
+    </LanguageContext.Provider>
+  );
 };
 
 export default App;
