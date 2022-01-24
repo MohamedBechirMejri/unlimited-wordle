52695446f441f3bd1c16dfaad24a7cc7ec11b8c8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-24 05:36:21 +0100

    show keys status on keyboard

--- src/Components/Main/Keyboard.tsx
+++ src/Components/Main/Keyboard.tsx
@@ -5,11 +5,18 @@ const Keyboard = ({
   submitWord,
   removeKey,
   addKey,
+  keysStatus,
 }: {
   submitWord: () => void;
   removeKey: () => void;
-  addKey: (arg0: string) => void;
+  addKey: (key: string) => void;
+  keysStatus: {
+    correct: string[];
+    misplaced: string[];
+    incorrect: string[];
+  };
 }) => {
+  const { correct, incorrect, misplaced } = keysStatus;
   return (
     <div>
       {layouts.en.map((row, rowIndex) => (
@@ -17,6 +24,17 @@ const Keyboard = ({
           {row.map((key, keyIndex) => (
             <button
               key={keyIndex}
+              className={`"
+                ${
+                  correct.includes(key)
+                    ? "correct"
+                    : misplaced.includes(key)
+                    ? "misplaced"
+                    : incorrect.includes(key)
+                    ? "incorrect"
+                    : null
+                }
+              "`}
               onClick={() => {
                 key === "enter"
                   ? submitWord()
