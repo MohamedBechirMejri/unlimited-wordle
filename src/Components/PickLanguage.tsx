23ec57caee9b21043e153a303c26600c0e470255
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-24 08:29:05 +0100

    add some styling for languague screen

--- src/Components/PickLanguage.tsx
+++ src/Components/PickLanguage.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
 const PickLanguage = (props: { setLanguage: (arg0: string) => void }) => (
-  <div>
-    <h1>Welcome To Unlimited Wordle!</h1>
-    <p>Please select your language:</p>
-    <div>
-      <button onClick={() => props.setLanguage("ar")}>العربية</button>
-      <button onClick={() => props.setLanguage("en")}>English</button>
-      <button onClick={() => props.setLanguage("fr")}>Français</button>
+  <div className="flex flex-col items-center justify-center w-screen h-screen gap-2">
+    <h1 className="font-bold ">Welcome To Unlimited Wordle!</h1>
+    <p className="text-sm">Please select your language:</p>
+    <div className="flex flex-col gap-2">
+      <button
+        className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
+        onClick={() => props.setLanguage("ar")}
+      >
+        العربية
+      </button>
+      <button
+        className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
+        onClick={() => props.setLanguage("en")}
+      >
+        English
+      </button>
+      <button
+        className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
+        onClick={() => props.setLanguage("fr")}
+      >
+        Français
+      </button>
     </div>
   </div>
 );
