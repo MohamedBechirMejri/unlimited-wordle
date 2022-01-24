2836efac9e45b953e53b5c09d2e25853f4f7dc01
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-24 16:56:35 +0100

    limit board size

--- src/Components/Main/GameBoard.tsx
+++ src/Components/Main/GameBoard.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-const GameBoard = ({ attempts }: { attempts: object }) => {
+const GameBoard = ({
+  attempts,
+  currentAttempt,
+}: {
+  attempts: object;
+  currentAttempt: number;
+}) => {
   return (
-    <div className="w-[80vw] h-[80vw] bg-sky-500 border-2 border-sky-200  flex flex-col items-center justify-center rounded-sm text-white uppercase font-bold overflow-hidden shadow text-3xl">
-      {Object.values(attempts).map((attempt, i) => (
-        <div key={i} className="flex items-center justify-center w-full h-1/6">
+    <div className="w-[80vw] h-[80vw] bg-sky-500 border-2 border-sky-200  flex flex-col items-center justify-center rounded-sm text-white uppercase font-bold overflow-hidden shadow text-3xl max-w-[500px] max-h-[500px]">
+      {Object.values(attempts).map((attempt, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-center w-full h-1/6"
+        >
           {attempt.letters.map((letter: string, i: number) => {
             return (
               <div
@@ -12,8 +21,8 @@ const GameBoard = ({ attempts }: { attempts: object }) => {
                 className="flex items-center justify-center bg-gradient-to-b from-sky-400  h-full border-[.1px] border-sky-200 shadow "
                 style={{
                   width: `${100 / attempt.letters.length}%`,
-                  background:
-                    attempt.statuses[i] === "correct" ? "crimson" : "",
+                  // background:
+                  //   attempt.statuses[i] === "correct" ? "crimson" : "",
                 }}
               >
                 <p
@@ -27,6 +36,7 @@ const GameBoard = ({ attempts }: { attempts: object }) => {
                         : attempt.statuses[i] === "misplaced"
                         ? "#ffe000"
                         : "",
+                    // if index === currentAttempt => animate with delay * i
                   }}
                 >
                   {letter}
