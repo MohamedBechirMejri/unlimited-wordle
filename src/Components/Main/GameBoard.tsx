de0f4e7805083e4d28bd04bcdf0050a29edc2d4f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-24 09:40:05 +0100

    more styling

--- src/Components/Main/GameBoard.tsx
+++ src/Components/Main/GameBoard.tsx
@@ -2,20 +2,36 @@ import React from "react";
 
 const GameBoard = ({ attempts }: { attempts: object }) => {
   return (
-    <div className="w-[85vw] bg-sky-500 border-2 border-sky-200  flex flex-col items-center justify-center rounded-sm text-white uppercase font-bold overflow-hidden">
+    <div className="w-[80vw] h-[80vw] bg-sky-500 border-2 border-sky-200  flex flex-col items-center justify-center rounded-sm text-white uppercase font-bold overflow-hidden shadow text-3xl">
       {Object.values(attempts).map((attempt, i) => (
-        <div key={i} className="flex items-center justify-center w-full">
+        <div key={i} className="flex items-center justify-center w-full h-1/6">
           {attempt.letters.map((letter: string, i: number) => {
             return (
-              <p
+              <div
                 key={i}
-                className={` ${attempt.statuses[i]} flex items-center justify-center bg-gradient-to-b from-sky-400  border-[.1px] border-sky-200 `}
+                className="flex items-center justify-center bg-gradient-to-b from-sky-400  h-full border-[.1px] border-sky-200 shadow "
                 style={{
                   width: `${100 / attempt.letters.length}%`,
+                  background:
+                    attempt.statuses[i] === "correct" ? "crimson" : "",
                 }}
               >
-                {letter}
-              </p>
+                <p
+                  className="flex items-center justify-center w-full h-full bg-gradient-to-b from-sky-400 "
+                  style={{
+                    borderRadius:
+                      attempt.statuses[i] === "misplaced" ? "100%" : "",
+                    background:
+                      attempt.statuses[i] === "correct"
+                        ? "crimson"
+                        : attempt.statuses[i] === "misplaced"
+                        ? "#ffe000"
+                        : "",
+                  }}
+                >
+                  {letter}
+                </p>
+              </div>
             );
           })}
         </div>
