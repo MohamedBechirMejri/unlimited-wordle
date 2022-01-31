fa8564c44081fed0a876c142c5cc728787a6d368
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-31 05:09:54 +0100

    show congrats if word is found

--- src/Components/Main/GameOver.tsx
+++ src/Components/Main/GameOver.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-const GameOver = ({ playAgain }: { playAgain: () => void }) => {
+const GameOver = ({
+  playAgain,
+  isGameWon,
+}: {
+  playAgain: () => void;
+  isGameWon: boolean;
+}) => {
   return (
     <div>
-      <h1>Game Over</h1>
-      <button onClick={playAgain}>Play Again</button>
+      <h1>{isGameWon ? "Good Job!" : "Game Over"}</h1>
+      <button onClick={playAgain}>
+        {isGameWon ? "Next Word" : "Play Again"}
+      </button>
     </div>
   );
 };
