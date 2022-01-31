641de3738cc2d1681f7ee393c365d171df6c3822
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-31 06:35:22 +0100

    add timer component

---
+++ src/Components/Main/Timer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Timer = ({ streak }: { streak: number }) => {
+  return (
+    <div>
+      <h2>Time Left: ♾️</h2>
+      <h2>Win Streak: {streak}</h2>
+    </div>
+  );
+};
+
+export default Timer;
