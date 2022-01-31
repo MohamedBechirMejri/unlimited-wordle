2f7243d84245d065d8a57312d25ec2568bc5df51
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-31 05:10:17 +0100

    import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import PickLanguage from "./Components/PickLanguage";
+import { LanguageContext } from "./Utils/LanguageContext";
+import Header from "./Components/Header";
+import Info from "./Components/Info";
+import Leaderboard from "./Components/Leaderboard";
+import Settings from "./Components/Settings";
+import Main from "./Components/Main";
 const App = () => {
   const [language, setLanguage] = useState("");
-  const LanguageContext = React.createContext(language);
+  const [page, setPage] = useState("");
 
   return (
     <LanguageContext.Provider value={language}>
       <div className="">
-        {language && <PickLanguage setLanguage={setLanguage} />}
+        {!language && <PickLanguage setLanguage={setLanguage} />}
+        {language && <Header page={page} setPage={setPage} />}
+        {page === "info" ? (
+          <Info />
+        ) : page === "leaderboard" ? (
+          <Leaderboard />
+        ) : page === "settings" ? (
+          <Settings language={language} setLanguage={setLanguage} />
+        ) : (
+          language && <Main />
+        )}
       </div>
     </LanguageContext.Provider>
   );
