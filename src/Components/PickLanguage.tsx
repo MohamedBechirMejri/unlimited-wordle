72072e18ed11f803a4dee31eb18a133c3185fc58
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-03 09:52:24 +0100

    refactor: :art: move code into own function

--- src/Components/PickLanguage.tsx
+++ src/Components/PickLanguage.tsx
@@ -18,31 +18,36 @@ const PickLanguage = ({
     colorBlindMode: boolean;
     challengeMode: boolean;
   }) => void;
-}) => (
-  <div className="flex flex-col items-center justify-center w-screen h-screen gap-2">
-    <h1 className="font-bold ">Welcome To Unlimited Wordle!</h1>
-    <p className="text-sm">Please select your language:</p>
-    <div className="flex flex-col gap-2">
-      <button
-        className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
-        onClick={() => setSettings({ ...settings, language: "ar" })}
-      >
-        العربية
-      </button>
-      <button
-        className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
-        onClick={() => setSettings({ ...settings, language: "en" })}
-      >
-        English
-      </button>
-      <button
-        className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
-        onClick={() => setSettings({ ...settings, language: "fr" })}
-      >
-        Français
-      </button>
+}) => {
+  const setLanguage = (lang: string) => {
+    setSettings({ ...settings, language: lang });
+  };
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen gap-2">
+      <h1 className="font-bold ">Welcome To Unlimited Wordle!</h1>
+      <p className="text-sm">Please select your language:</p>
+      <div className="flex flex-col gap-2">
+        <button
+          className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
+          onClick={() => setLanguage("ar")}
+        >
+          العربية
+        </button>
+        <button
+          className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
+          onClick={() => setLanguage("en")}
+        >
+          English
+        </button>
+        <button
+          className="px-2 py-1 text-white transition-all rounded bg-slate-800 active:scale-95"
+          onClick={() => setLanguage("fr")}
+        >
+          Français
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PickLanguage;
