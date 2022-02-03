7431ccb08a0e51f6e35260e8f3be422bef8fd52b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-03 12:16:03 +0100

    make settings buttons functional

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -20,9 +20,9 @@ const Settings = ({
   }) => void;
 }) => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen w-scren">
       <h1>Settings</h1>
-      <div>
+      <div className="flex items-center justify-start">
         <h2>Language</h2>
         <select
           name="lang"
@@ -35,21 +35,46 @@ const Settings = ({
           <option value="fr">Français</option>
         </select>
       </div>
-      <div>
+      <div className="flex items-center justify-start">
         <h2>Dark Theme</h2>
-        <button>Off</button>
+        <button
+          onClick={() =>
+            setSettings({ ...settings, darkTheme: !settings.darkTheme })
+          }
+        >
+          {settings.darkTheme ? "On" : "Off"}
+        </button>
       </div>
-      <div>
+      <div className="flex items-center justify-start">
         <h2>Sound</h2>
-        <button>On</button>
+        <button
+          onClick={() => setSettings({ ...settings, sound: !settings.sound })}
+        >
+          {settings.sound ? "On" : "Off"}
+        </button>
       </div>
-      <div>
+      <div className="flex items-center justify-start">
         <h2>Color Blind Mode</h2>
-        <button>On</button>
+        <button
+          onClick={() =>
+            setSettings({
+              ...settings,
+              colorBlindMode: !settings.colorBlindMode,
+            })
+          }
+        >
+          {settings.colorBlindMode ? "On" : "Off"}
+        </button>
       </div>
-      <div>
+      <div className="flex items-center justify-start">
         <h2>Challenge Mode</h2>
-        <button>Off</button>
+        <button
+          onClick={() =>
+            setSettings({ ...settings, challengeMode: !settings.challengeMode })
+          }
+        >
+          {settings.challengeMode ? "On" : "Off"}
+        </button>
       </div>
       <a href="https://github.com/" target={"_blank"} rel="noreferrer">
         Feedback!
