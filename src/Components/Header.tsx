90a95c0840bda7fae6b4f548a216fafe96a6e94d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-05 06:49:51 +0100

    tweak title

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -14,9 +14,16 @@ const Header = ({
   setPage: (arg0: string) => void;
 }) => {
   return (
-    <div className="fixed flex items-center justify-between w-screen max-w-[500px] top-0 left-1/2 -translate-x-1/2 z-50">
-      <h1>UW</h1>
-      <div>
+    <div className="fixed flex items-center justify-between w-screen max-w-[500px] top-0 left-1/2 -translate-x-1/2 z-50 text-4xl p-2 transition-all">
+      <h1
+        className="flex flex-col justify-center text-base text-transparent"
+        style={{
+          WebkitTextStroke: "1px black",
+        }}
+      >
+        Unlimited <span>Wordle</span>{" "}
+      </h1>
+      <div className="flex items-center gap-2 ">
         {" "}
         <button
           onClick={() => {
