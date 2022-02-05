1a0f3d586eba96789ef2b7fffcc3e91c41215b17
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-02-05 16:33:38 +0100

    remove timer

--- src/Components/Main/Timer.tsx
+++ src/Components/Main/Timer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Timer = ({ streak }: { streak: number }) => {
   return (
     <div>
-      <h2>Time Left: ♾️</h2>
+      {/* <h2>Time Left: ♾️</h2> */}
       <h2>Win Streak: {streak}</h2>
     </div>
   );
